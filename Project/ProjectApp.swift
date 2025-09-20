967e26d7a4da3100009c05c8bc42379ee845070c
MEHUL-CHANDRAKAR <mehulchandrakar80@gmail.com>
2025-09-20 23:51:45 +0530

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by MEHUL CHANDRAKAR on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
